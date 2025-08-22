b9ec370ada30b74618dc6679d1c11d8ed9392e25
ZahoorOnly <i.am.zahoor.a@outlook.com>
2025-08-22 12:14:35 +0500

    feat: made simple functions to extract min temp max temp humidity month day year etc.

---
+++ weatherman.js
@@ -0,0 +1,27 @@
+import { weatherData } from "./weather-data.js";
+
+const extractYear = (item) => {
+  if (item.date) return parseInt(item.date.split("-")[0]);
+};
+const extractMonth = (item) => {
+  if (item.date) return parseInt(item.date.split("-")[1]);
+};
+const extractDay = (item) => {
+  if (item.date) return parseInt(item.date.split("-")[2]);
+};
+const extractMaxTemperature = (item) => {
+  if (item.maxTemperatureC) return parseInt(item.maxTemperatureC);
+};
+const extractMinTemperature = (item) => {
+  if (item.minTemperatureC) return parseInt(item.minTemperatureC);
+};
+const extractHumidity = (item) => {
+  if (item.maxHumidity) return parseInt(item.maxHumidity);
+};
+
+console.log(extractYear(weatherData[0]));
+console.log(extractMonth(weatherData[0]));
+console.log(extractDay(weatherData[0]));
+console.log(extractMaxTemperature(weatherData[0]));
+console.log(extractMinTemperature(weatherData[0]));
+console.log(extractHumidity(weatherData[0]));
