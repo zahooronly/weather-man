d267d0c9f6536e2f1e8baa886e7d6fb05bffcb01
ZahoorOnly <i.am.zahoor.a@outlook.com>
2025-08-22 19:05:14 +0500

    changed function from monthlyValue to getMonthlyData and others same way.
    updated yearly variable's structure
    
    restructured result in monthlyData function

--- weatherman.js
+++ weatherman.js
@@ -6,7 +6,7 @@ import {
   monthNumberToName,
 } from "./utils.js";
 
-const monthlyValue = (yearSlashMonth) => {
+const getMonthlyData = (yearSlashMonth) => {
   const year = parseInt(yearSlashMonth.split("/")[0]);
   const month = parseInt(yearSlashMonth.split("/")[1]);
   const result = weatherData.filter((item) => {
@@ -19,7 +19,7 @@ const monthlyValue = (yearSlashMonth) => {
   return result;
 };
 
-const yearlyData = (year) => {
+const getYearlyStats = (year) => {
   let stats = {
     maxTemp: {
       value: 0,
@@ -34,24 +34,25 @@ const yearlyData = (year) => {
       index: 0,
     },
   };
-  const yearly = weatherData.filter((item) => year == extractYear(item));
-  yearly.map((item, index) => {
-    let maxTemp = parseInt(item.maxTemperatureC);
-    let minTemp = parseInt(item.minTemperatureC);
-    let mostHumidity = parseInt(item.maxHumidity);
-    if (maxTemp > stats.maxTemp.value) {
-      stats.maxTemp.value = maxTemp;
-      stats.maxTemp.index = index;
-    }
-    if (minTemp < stats.minTemp.value) {
-      stats.minTemp.value = minTemp;
-      stats.minTemp.index = index;
-    }
-    if (mostHumidity > stats.mostHumidity.value) {
-      stats.mostHumidity.value = mostHumidity;
-      stats.mostHumidity.index = index;
-    }
-  });
+  weatherData
+    .filter((item) => year == extractYear(item))
+    .map((item, index) => {
+      let maxTemp = parseInt(item.maxTemperatureC);
+      let minTemp = parseInt(item.minTemperatureC);
+      let mostHumidity = parseInt(item.maxHumidity);
+      if (maxTemp > stats.maxTemp.value) {
+        stats.maxTemp.value = maxTemp;
+        stats.maxTemp.index = index;
+      }
+      if (minTemp < stats.minTemp.value) {
+        stats.minTemp.value = minTemp;
+        stats.minTemp.index = index;
+      }
+      if (mostHumidity > stats.mostHumidity.value) {
+        stats.mostHumidity.value = mostHumidity;
+        stats.mostHumidity.index = index;
+      }
+    });
   let result = [
     `Highest: ${stats.maxTemp.value}C on ${monthNumberToName(
       extractMonth(weatherData[stats.maxTemp.index])
@@ -67,16 +68,11 @@ const yearlyData = (year) => {
 };
 
 const monthlyData = (yearSlashMonth) => {
-  const monthlyValues = monthlyValue(yearSlashMonth);
+  const monthlyValues = getMonthlyData(yearSlashMonth);
   let monthlyTotal = {
     totalHighTemp: 0,
     totalLowTemp: 0,
     totalHumidity: 0,
-  };
-  let result = {
-    averageHighTemp: 0,
-    averageLowTemp: 0,
-    averageHumidity: 0,
   };
   if (monthlyValues) {
     monthlyValues.map((item) => {
@@ -87,18 +83,16 @@ const monthlyData = (yearSlashMonth) => {
   }
   const totalDays = monthlyValues.length;
 
-  result.averageHighTemp = Math.floor(monthlyTotal.totalHighTemp / totalDays);
-  result.averageLowTemp = Math.floor(monthlyTotal.totalLowTemp / totalDays);
-  result.averageHumidity = Math.floor(monthlyTotal.totalHumidity / totalDays);
-
   return [
-    `Highest average: ${result.averageHighTemp}C`,
-    `Lowest average: ${result.averageLowTemp}C`,
-    `Average Mean Humidity: ${result.averageHumidity}%`,
+    `Highest average: ${Math.floor(monthlyTotal.totalHighTemp / totalDays)}C`,
+    `Lowest average: ${Math.floor(monthlyTotal.totalLowTemp / totalDays)}C`,
+    `Average Mean Humidity: ${Math.floor(
+      monthlyTotal.totalHumidity / totalDays
+    )}%`,
   ];
 };
 const dailyData = (yearSlashMonth) => {
-  const monthlyValues = monthlyValue(yearSlashMonth);
+  const monthlyValues = getMonthlyData(yearSlashMonth);
   let output = [];
   monthlyValues.map((item) => {
     output.push([
@@ -112,6 +106,6 @@ const dailyData = (yearSlashMonth) => {
   return output;
 };
 
-console.log(yearlyData(2010));
+console.log(getYearlyStats(2010));
 console.log(monthlyData("2010/4"));
 console.log(dailyData("2010/4"));
