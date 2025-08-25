b1f9f7bb53ed4ea359b699ee04ab0c2aecad90d9
ZahoorOnly <i.am.zahoor.a@outlook.com>
2025-08-25 13:45:58 +0500

    fix: created constants file separate.

--- utils.js
+++ utils.js
@@ -1,21 +1,8 @@
+import { months } from "./constants.js";
 export const extractYear = (item) => parseInt(item?.date?.split("-")[0]);
 export const extractMonth = (item) => parseInt(item?.date?.split("-")[1]);
 export const extractDay = (item) => parseInt(item?.date?.split("-")[2]);
 
 export const getMonthNameFromNumber = (n) => {
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
   return months[n - 1];
 };
